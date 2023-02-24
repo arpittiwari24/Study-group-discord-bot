2a7de014de8233eb80350fd8cf5845bee7ecc537
Arrpit Tiwari <atofficial2410@gmail.com>
2023-02-24 14:07:42 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"main.go/bot"
+	"main.go/config"
+)
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
